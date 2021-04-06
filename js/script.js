be94ce9f7bedebecde8d5e9e94a2ae6cb8905483
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-04-06 17:21:24 +0200

    Milestone 2 inserimento serie tv
    Necessaria modifca original_language == ja, ko sostituite con jp ed kr per poter usare correttamente https://www.countryflags.io

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ var app = new Vue ({
     el: '#root',
     data: {
         searchInput: '',
-        moviesArray: []
+        moviesArray: [],
+        seriesArray: []
     },
     methods: {
-        searchMovies: function() {
+        searchMoviesAndSeries: function() {
             axios
                 .get('https://api.themoviedb.org/3/search/movie?api_key=02911b373bf1ff255f89a4eb00028d32', { 
                     params: {
@@ -18,6 +19,33 @@ var app = new Vue ({
                     app.moviesArray.forEach(element => {
                         if(element.original_language == 'en') {
                             element.original_language = 'gb';
+                        }
+                        if (element.original_language == 'ja') {
+                            element.original_language = 'jp';
+                        }
+                        if (element.original_language == 'ko') {
+                            element.original_language = 'kr';
+                        }
+                    });
+                })
+            axios
+                .get('https://api.themoviedb.org/3/search/tv?api_key=02911b373bf1ff255f89a4eb00028d32', {
+                    params: {
+                        query: app.searchInput,
+                        language: 'it-IT'
+                    }
+                })
+                .then(function (result) {
+                    app.seriesArray = result.data.results;
+                    app.seriesArray.forEach(element => {
+                        if (element.original_language == 'en') {
+                            element.original_language = 'gb';
+                        }
+                        if (element.original_language == 'ja') {
+                            element.original_language = 'jp';
+                        }
+                        if (element.original_language == 'ko') {
+                            element.original_language = 'kr';
                         }
                     });
                 })
