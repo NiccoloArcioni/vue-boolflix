31746dbe5ff7400c74d8dc1c5e5ff13eb365af0c
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-04-07 12:06:19 +0200

    Milestone 5 inserimento method getDetails
    effettua due chiamate axios per ottenere generi e cast, successivamente inseriti in movieArray e seriesArray (da inserire stampa a schermo)

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ var app = new Vue ({
                 .then(function (result) {
                     app.moviesArray = result.data.results;
                     app.modifyLanguages(app.moviesArray);
+                    app.getDetails(app.moviesArray);
                 })
                 axios
                 .get('https://api.themoviedb.org/3/search/tv?', {
@@ -28,14 +29,47 @@ var app = new Vue ({
                         api_key: '02911b373bf1ff255f89a4eb00028d32',
                         query: app.searchInput,
                         language: 'it-IT'
-                    }
+                    } 
                 })
                 .then(function (result) {
                     app.seriesArray = result.data.results;
                     app.modifyLanguages(app.seriesArray);
+                    app.getDetails(app.seriesArray);
                 })
                 this.searchInput = '';
             }
+        },
+        getDetails: function(array) {
+            var movieOrTvSeries;
+            if ('original_title' in array[0]) {
+                movieOrTvSeries = 'movie';
+            } else {
+                movieOrTvSeries = 'tv';
+            }
+            array.forEach(element => {
+                /* genres */
+                axios
+                    .get(`https://api.themoviedb.org/3/${movieOrTvSeries}/${element.id}?`, {
+                        params: {
+                            api_key: 'e99307154c6dfb0b4750f6603256716d',
+                            language: 'it-IT'
+                        }
+                    })
+                    .then(function (result) {
+                        element.genres = result.data.genres;
+                    })
+                /* cast */
+                axios
+                    .get(`https://api.themoviedb.org/3/${movieOrTvSeries}/${element.id}/credits?`, {
+                        params: {
+                            api_key: 'e99307154c6dfb0b4750f6603256716d',
+                            language: 'it-IT'
+                        }
+                    })
+                    .then(function (result) {
+                        element.cast = result.data.cast;
+                    })
+            })
         },
         showInfo: function(index) {
             let movie_tvSeries = document.getElementById(`${index}`);
