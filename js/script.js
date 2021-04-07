c3e7fa1539fe696609a038a5cfe5c2260ed9944b
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-04-07 10:06:32 +0200

    Modify: inserito method modifyLanguages ed aggiunte stelle vuote a voto

--- js/script.js
+++ js/script.js
@@ -11,59 +11,54 @@ var app = new Vue ({
         searchMoviesAndSeries: function() {
             if(this.searchInput.length >= 1) {
                 axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=02911b373bf1ff255f89a4eb00028d32', { 
+                .get('https://api.themoviedb.org/3/search/movie?', { 
                     params: {
+                        api_key: '02911b373bf1ff255f89a4eb00028d32',
                         query: app.searchInput,
                         language: 'it-IT'
                     } 
                 })
                 .then(function (result) {
                     app.moviesArray = result.data.results;
-                    app.moviesArray.forEach(element => {
-                        if(element.original_language == 'en') {
-                            element.original_language = 'gb';
-                        }
-                        if (element.original_language == 'ja') {
-                            element.original_language = 'jp';
-                        }
-                        if (element.original_language == 'ko') {
-                            element.original_language = 'kr';
-                        }
-                    });
+                    app.modifyLanguages(app.moviesArray);
                 })
                 axios
-                .get('https://api.themoviedb.org/3/search/tv?api_key=02911b373bf1ff255f89a4eb00028d32', {
+                .get('https://api.themoviedb.org/3/search/tv?', {
                     params: {
+                        api_key: '02911b373bf1ff255f89a4eb00028d32',
                         query: app.searchInput,
                         language: 'it-IT'
                     }
                 })
                 .then(function (result) {
                     app.seriesArray = result.data.results;
-                    app.seriesArray.forEach(element => {
-                        if (element.original_language == 'en') {
-                            element.original_language = 'gb';
-                        }
-                        if (element.original_language == 'ja') {
-                            element.original_language = 'jp';
-                        }
-                        if (element.original_language == 'ko') {
-                            element.original_language = 'kr';
-                        }
-                    });
+                    app.modifyLanguages(app.seriesArray);
                 })
                 this.searchInput = '';
             }
         },
         showInfo: function(index) {
-            let movie = document.getElementById(`${index}`);
-            movie.querySelector('.ms_main_poster').classList.remove('active');
-            movie.querySelector('.ms_infos').classList.add('active');
+            let movie_tvSeries = document.getElementById(`${index}`);
+            movie_tvSeries.querySelector('.ms_main_poster').classList.remove('active');
+            movie_tvSeries.querySelector('.ms_infos').classList.add('active');
         },
-        closeInfo: function (index) {
-            let movie = document.getElementById(`${index}`);
-            movie.querySelector('.ms_infos').classList.remove('active');
-            movie.querySelector('.ms_main_poster').classList.add('active');
+        closeInfo: function(index) {
+            let movie_tvSeries = document.getElementById(`${index}`);
+            movie_tvSeries.querySelector('.ms_infos').classList.remove('active');
+            movie_tvSeries.querySelector('.ms_main_poster').classList.add('active');
+        },
+        modifyLanguages: function(array) {
+            array.forEach(element => {
+                if (element.original_language == 'en') {
+                    element.original_language = 'gb';
+                }
+                if (element.original_language == 'ja') {
+                    element.original_language = 'jp';
+                }
+                if (element.original_language == 'ko') {
+                    element.original_language = 'kr';
+                }
+            })
         }
     }
 })
